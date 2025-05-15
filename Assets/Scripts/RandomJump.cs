73e2389c17c0d3313bb06e78bd36eb23d15bde21
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2025-05-15 22:27:51 +0300

    доделал

--- Assets/Scripts/RandomJump.cs
+++ Assets/Scripts/RandomJump.cs
@@ -7,9 +7,16 @@ public class RandomJump : MonoBehaviour
     [SerializeField] private float _jumpDuration = 1f;
     [SerializeField] private float _jumpPower = 1f;
     [SerializeField] private int _numJumps = 1;
+    [SerializeField] private float _rotationDuration = 2f;
+    [SerializeField] private int _loops = -1;
+    [SerializeField] private Vector3 _rotationAxis = new Vector3(0, 360, 360);
 
     private void Start()
     {
+        transform.DORotate(_rotationAxis, _rotationDuration, RotateMode.LocalAxisAdd)
+            .SetLoops(_loops, LoopType.Restart)
+            .SetEase(Ease.Linear);
+
         JumpToRandomPoint();
     }
 
